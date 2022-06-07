d667e221e062268b0d4c552ee7b7200f69b3085b
ACRei <lhh@live.com>
2022-06-07 22:51:44 +0800

    fix

--- SS_Encryption_Test/Ubuntu.sh
+++ SS_Encryption_Test/Ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-test_method='aes-128-gcm aes-256-gcm chacha20-ietf chacha20-ietf-poly1305 rc4-md5 xchacha20-ietf-poly1305'
+test_method='aes-128-gcm aes-256-gcm chacha20 chacha20-ietf chacha20-ietf-poly1305 rc4-md5 xchacha20-ietf-poly1305'
 
 gen_ss_json(){
 	cat <<-EOF > $PWD/ss.json
@@ -16,30 +16,28 @@ gen_ss_json(){
 	EOF
 }
 
-main(){
-    set -x
-    dd if=/dev/zero of=/var/www/html/test.img bs=1M count=1024
-    for method in $test_method
-    do
-        gen_ss_json $method
-        ss-server -c $PWD/ss.json >/dev/null 2>&1 &
-        ss-local -c  $PWD/ss.json >/dev/null 2>&1 &
-        curl --socks5 127.0.0.1 127.0.0.1/test.img -o /dev/null 2>&1 | tee $PWD/${method}_curl_info
-        killall ss-server ss-local
-    done
-    set +x
-    echo
-    echo -----------------RESULT-----------------
-    for method in $test_method
-    do
-        echo "          Method $method speed: `cat ${method}_curl_info |grep '100'|awk -F ' ' '{print $(NF-5)}'`"
-    done
-    echo -----------------RESULT-----------------
-    echo
-    set -x
-    rm -f /var/www/html/test.img $PWD/ss.json $PWD/*curl_info
-}
-
 apt install nginx shadowsocks-libev
 
-main
+sleep 5s
+
+set -x
+dd if=/dev/zero of=/var/www/html/test.img bs=1M count=1024
+for method in $test_method
+do
+    gen_ss_json $method
+    ss-server -c $PWD/ss.json >/dev/null 2>&1 &
+    ss-local -c  $PWD/ss.json >/dev/null 2>&1 &
+    curl --socks5 127.0.0.1 127.0.0.1/test.img -o /dev/null 2>&1 | tee $PWD/${method}_curl_info
+    killall ss-server ss-local
+done
+set +x
+echo
+echo -----------------RESULT-----------------
+for method in $test_method
+do
+    echo "          Method $method speed: `cat ${method}_curl_info |grep '100'|awk -F ' ' '{print $(NF-5)}'`"
+done
+echo -----------------RESULT-----------------
+echo
+set -x
+rm -f /var/www/html/test.img $PWD/ss.json $PWD/*curl_info
